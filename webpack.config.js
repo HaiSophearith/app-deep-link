1b6cc9a60170a33e44e005e2c6b0a409e0992b8c
hai sophearith <sophearithhai@gmail.com>
2024-05-07 10:10:34 +0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const TerserWebpackPlugin = require('terser-webpack-plugin');
 module.exports = {
-    mode: "none",
-    entry: {
-        'app-deep-link': './src/index.js',
-        'app-deep-link.min': './src/index.js'
+  mode: "none",
+  entry: {
+    "app-deep-link": "./src/index.js",
+    "app-deep-link.min": "./src/index.js",
+  },
+  output: {
+    filename: (chunkData) => {
+      return chunkData.chunk.name === "app-deep-link.min"
+        ? "public/[name].min.js"
+        : "public/[name].js";
     },
-    output: {
-        filename: (chunkData) => {
-            return chunkData.chunk.name === 'app-deep-link.min' ? 'public/[name].js' : '[name].js';
-        },
-        library: 'AppDeepLink',
-        libraryTarget: 'umd',
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserWebpackPlugin({
-                include: /\.min\.js$/
-            })
-        ]
-    }
+    library: "AppDeepLink",
+    libraryTarget: "umd",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        include: /\.min\.js$/,
+      }),
+    ],
+  },
 };
